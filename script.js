55cde127e199ed58915d368510647ad8262a30ba
rayan2228 <rayanh.dev@gmail.com>
2025-03-13 10:08:35 +0600

    initialize script with color input elements and notification setup

---
+++ script.js
@@ -0,0 +1,7 @@
+const colorInput = document.getElementById("colorInput");
+const pickedColor = document.getElementById("pickedColor");
+const complementaryColor = document.getElementById("complementaryColor");
+const saveColor = document.getElementById("saveColor");
+const savedColors = document.getElementById("savedColors");
+const savedColorsList = document.querySelector(".saved-colors-list");
+const toastNotification = document.getElementById("toast");
