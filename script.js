6aaf3ea8822bf210096d3f6f5e5e59bc569cb7c1
rayan2228 <rayanh.dev@gmail.com>
2025-03-13 10:27:48 +0600

    update color input default value and implement color display functionality

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const colorInput = document.getElementById("colorInput");
 const pickedColor = document.getElementById("pickedColor");
-const complementaryColor = document.getElementById("complementaryColor");
+const complementaryColorElm = document.getElementById("complementaryColor");
 const saveColor = document.getElementById("saveColor");
 const savedColors = document.getElementById("savedColors");
 const savedColorsList = document.querySelector(".saved-colors-list");
 const toastNotification = document.getElementById("toast");
+
+colorInput.addEventListener("input", () => {
+  const selectedColor = colorInput.value;
+  const complementaryColor = getComplementaryColor(selectedColor);
+  updateColorDisplay(selectedColor, complementaryColor);
+});
+
+function getComplementaryColor(selectedColor) {
+    
+}
+
+function updateColorDisplay(selectedColor, complementaryColor) {
+  pickedColor.style.backgroundColor = selectedColor;
+  complementaryColorElm.style.backgroundColor = complementaryColor;
+}
