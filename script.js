c761b5ab8a16d138b7e734553aecdac88fa5437d
rayan2228 <rayanh.dev@gmail.com>
2025-03-13 11:54:06 +0600

    refactor color handling and improve saved colors display

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
-const colorInput = document.getElementById("colorInput");
-const pickedColor = document.getElementById("pickedColor");
+const colorInputElm = document.getElementById("colorInput");
+const pickedColorElm = document.getElementById("pickedColor");
 const complementaryColorElm = document.getElementById("complementaryColor");
-const saveColor = document.getElementById("saveColor");
-const savedColors = document.getElementById("savedColors");
-const savedColorsList = document.querySelector(".saved-colors-list");
-const toastNotification = document.getElementById("toast");
+const saveColorBtn = document.getElementById("saveColor");
+const savedColorsElm = document.getElementById("savedColors");
+const savedColorsListElm = document.querySelector(".saved-colors-list");
+const toastNotificationElm = document.getElementById("toast");
+let pickedColorCode = "#ffffff";
+let complementaryColorCode = "#000000";
 
-colorInput.addEventListener("input", () => {
-  const selectedColor = colorInput.value;
-  const complementaryColor = getComplementaryColor(selectedColor);
-  updateColorDisplay(selectedColor, complementaryColor);
+colorInputElm.addEventListener("input", () => {
+  pickedColorCode = colorInputElm.value;
+  complementaryColorCode = getComplementaryColor(pickedColorCode);
+  updateColorDisplay(pickedColorCode, complementaryColorCode);
 });
 
-function getComplementaryColor(selectedColor) {
-  const base = parseInt(selectedColor.slice(1), 16);
-  return `#${(0xffffff ^ base).toString(16).padStart(6, "0")}`;
-}
-
-function updateColorDisplay(selectedColor, complementaryColor) {
-  pickedColor.style.backgroundColor = selectedColor;
-  complementaryColorElm.style.backgroundColor = complementaryColor;
-}
-
-pickedColor.addEventListener("click", () =>
-  copyTheColor(pickedColor.style.backgroundColor)
+pickedColorElm.addEventListener("click", () =>
+  copyTheColor(pickedColorElm.style.backgroundColor)
 );
 
 complementaryColorElm.addEventListener("click", () =>
   copyTheColor(complementaryColorElm.style.backgroundColor)
 );
 
+saveColorBtn.addEventListener("click", savedColor);
+function getComplementaryColor(selectedColor) {
+  const base = parseInt(selectedColor.slice(1), 16);
+  return `#${(0xffffff ^ base).toString(16).padStart(6, "0")}`;
+}
+
+function updateColorDisplay(selectedColor, complementaryColor) {
+  pickedColorElm.style.backgroundColor = selectedColor;
+  complementaryColorElm.style.backgroundColor = complementaryColor;
+}
+
 function copyTheColor(target) {
   navigator.clipboard
     .writeText(target)
     .then(() => {
-      toastNotification.style.visibility = "visible";
-      toastNotification.style.opacity = 10;
+      toastNotificationElm.style.visibility = "visible";
+      toastNotificationElm.style.opacity = 10;
     })
-    .catch(() => (toastNotification.innerText = "failed to copy"))
+    .catch(() => (toastNotificationElm.innerText = "failed to copy"))
     .finally(() => {
       setTimeout(() => {
-        toastNotification.style.visibility = "hidden";
-        toastNotification.style.opacity = 0;
+        toastNotificationElm.style.visibility = "hidden";
+        toastNotificationElm.style.opacity = 0;
       }, 2000);
     });
+}
+
+function savedColor() {
+  const savedColorElm = document.createElement("div");
+  savedColorElm.classList.add("saved-color");
+  savedColorElm.innerHTML = `
+    <div style="background-color: ${pickedColorCode}">
+      <span class="color-label" >${pickedColorCode}</span>
+    </div>
+    <div  style="background-color: ${complementaryColorCode}">
+      <span class="color-label">${complementaryColorCode}</span>
+    </div>
+  `;
+  savedColorsListElm.prepend(savedColorElm);
 }
